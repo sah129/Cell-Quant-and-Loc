2039453a71fcec88e01f0347d9d8f13655f8614c
sah129 <55058535+sah129@users.noreply.github.com>
2020-05-19 14:56:13 -0400

    Add files via upload

--- ODonnell Cell Quant/src/display.R
+++ ODonnell Cell Quant/src/display.R
@@ -31,3 +31,63 @@ get_display_img <- function(df,membranes, col_membranes, vacuoles, col_vacuoles,
          vfont = c("sans serif", "bold"))
   }
 }
+
+
+get_final_pm_img <- function(mems, res)
+{
+  if(is.null(res$empty_cells))
+    r <- rmObjects(mems$membranes, res$empty_cells)
+  return(r)
+}
+
+
+get_final_vac_img <- function(vacs, res)
+{
+  vac_df <- drop_na(res$df["vacuoles"])
+  l <- length(table(vacuoles$vacuoles)[-1])
+  vac_list <- vector('list', l)
+  i=1
+  for(v in vac_df)
+  {
+    cv <- strsplit(v, ',')
+    for(vv in cv)
+    {
+      
+      for(vvv in vv) #lol
+      {
+        vac_list[i]=as.numeric(str_trim(vvv))
+        i = i + 1
+      }
+    }
+    vac_list <- unlist(vac_list)
+    removedvacs <- which(!(seq(1:length(table(vacuoles$vacuoles)[-1])) %in% vac_list))
+    
+  }
+  res_vacs <- rmObjects(vacs$vacuoles, removedvacs)
+  return(res_vacs)
+}
+
+
+tidy_up <- function(membranes,vacuoles,res)
+{
+  final_pm_img <- get_final_pm_img(membranes, res)
+  final_vac_img <- get_final_vac_img(vacuoles, res)
+  final_df <- renumerate_df(res$df)
+  list(membranes = final_pm_img, vacuoles = final_vac_img, df = final_df)
+}
+
+renumerate_df <- function(df)
+{
+  if(any(is.na(df['CellID'])))
+  {
+    df = drop_na(df)
+    message('renumerating cell ids')
+    df['CellID'] = seq(1:nrow(df))
+  }
+  message('reenumerating vacuoles')
+  df['vacuoles'] = seq((nrow(df)+1),2*nrow(df))
+  return(df)
+  
+}
+
+
